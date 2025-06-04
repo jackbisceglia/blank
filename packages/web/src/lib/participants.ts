cc852b6c7dabd179fe573d7a8d21d988c9c366ee
Jack Bisceglia <57922742+jackbisceglia@users.noreply.github.com>
2025-06-04 00:47:48 -0400

    fix: fix balance card sorting logic w/ unit test (bun:test), and balance card visual balance tweak (#16)

--- packages/web/src/lib/participants.ts
+++ packages/web/src/lib/participants.ts
@@ -10,3 +10,27 @@ export function getPayerFromParticipants(
 ) {
   return participants.find((p) => p.role === "payer")?.userId;
 }
+
+export type MemberBalanceTuple = readonly [Member, number];
+
+// Custom sort order:
+// 1. positive balances first, descending
+// 2. negative balances next, descending by absolute value
+// 3. zero balances last
+export function compareParticipantsCustomOrder(
+  [_memberA, a]: MemberBalanceTuple,
+  [_memberB, b]: MemberBalanceTuple
+) {
+  if (a === b) return 0;
+
+  // deprioritize 0s (shift right)
+  if (a === 0 && b !== 0) return 1;
+  if (b === 0 && a !== 0) return -1;
+
+  // if both neg, prioritize higher abs values (shift left)
+  if (a < 0 && b < 0) return Math.abs(b) - Math.abs(a);
+
+  // this means we have a mix of pos/neg, or both pos
+  // so we just push biggest values left
+  return b - a;
+}
