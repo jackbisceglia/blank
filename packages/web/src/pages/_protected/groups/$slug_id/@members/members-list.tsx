25f00d5cf4dd56351b7435c84a60de7e8b67db8a
jackbisceglia <jackbisceglia2000@gmail.com>
2025-07-25 21:01:16 -0400

    feat: finish implementing full members page

--- packages/web/src/pages/_protected/groups/$slug_id/@members/members-list.tsx
+++ packages/web/src/pages/_protected/groups/$slug_id/@members/members-list.tsx
@@ -16,36 +16,33 @@ import {
   getBalanceText,
 } from "@/components/utils";
 import { Hash } from "effect";
-import { ComponentProps, PropsWithChildren } from "react";
+import { ComponentProps, FormEvent, PropsWithChildren, useState } from "react";
+import { useUpdateMemberNickname } from "@/pages/_protected/@data/members";
+import { withToast } from "@/lib/toast";
+import { Input } from "@/components/ui/input";
+import { prevented } from "@/lib/utils";
+import * as v from "valibot";
+import { useAppForm } from "@/components/form";
+
+const constants = { nickname: "nickname" };
+const constraints = { nickname: { minLength: 1, maxLength: 32 } };
 
 const gradients = [
-  "bg-gradient-to-br from-[hsl(220,60%,60%)] to-[hsl(230,50%,40%)]",
-  "bg-gradient-to-br from-[hsl(210,65%,55%)] to-[hsl(240,55%,35%)]",
-  "bg-gradient-to-br from-[hsl(225,70%,50%)] to-[hsl(245,60%,30%)]",
-
-  "bg-gradient-to-br from-[hsl(250,60%,60%)] to-[hsl(260,50%,40%)]",
-  "bg-gradient-to-br from-[hsl(240,65%,55%)] to-[hsl(270,55%,35%)]",
-  "bg-gradient-to-br from-[hsl(260,70%,50%)] to-[hsl(280,60%,30%)]",
-
-  "bg-gradient-to-br from-[hsl(280,60%,60%)] to-[hsl(290,50%,40%)]",
-  "bg-gradient-to-br from-[hsl(300,65%,55%)] to-[hsl(320,55%,35%)]",
-  "bg-gradient-to-br from-[hsl(290,70%,50%)] to-[hsl(310,60%,30%)]",
-
-  "bg-gradient-to-br from-[hsl(320,65%,60%)] to-[hsl(340,55%,40%)]",
-  "bg-gradient-to-br from-[hsl(330,70%,55%)] to-[hsl(350,60%,35%)]",
-  "bg-gradient-to-br from-[hsl(340,75%,50%)] to-[hsl(360,65%,30%)]",
-
-  "bg-gradient-to-br from-[hsl(170,60%,60%)] to-[hsl(180,50%,40%)]",
-  "bg-gradient-to-br from-[hsl(160,65%,55%)] to-[hsl(190,55%,35%)]",
-  "bg-gradient-to-br from-[hsl(180,70%,50%)] to-[hsl(200,60%,30%)]",
-
-  "bg-gradient-to-br from-[hsl(150,60%,60%)] to-[hsl(160,50%,40%)]",
-  "bg-gradient-to-br from-[hsl(140,65%,55%)] to-[hsl(170,55%,35%)]",
-  "bg-gradient-to-br from-[hsl(160,70%,50%)] to-[hsl(180,60%,30%)]",
+  "bg-gradient-to-br from-[hsl(200,65%,60%)] to-[hsl(260,55%,45%)]",
+  "bg-gradient-to-tl from-[hsl(160,60%,65%)] to-[hsl(220,50%,50%)]",
+  "bg-gradient-to-tr from-[hsl(30,70%,65%)] to-[hsl(350,60%,45%)]",
+  "bg-gradient-to-bl from-[hsl(280,65%,60%)] to-[hsl(180,55%,50%)]",
+  "bg-gradient-to-tr from-[hsl(45,65%,65%)] to-[hsl(270,50%,45%)]",
+  "bg-gradient-to-tr from-[hsl(320,60%,60%)] to-[hsl(120,55%,50%)]",
+  "bg-gradient-to-br from-[hsl(190,65%,65%)] to-[hsl(340,60%,45%)]",
+  "bg-gradient-to-tl from-[hsl(60,60%,60%)] to-[hsl(240,55%,50%)]",
+  "bg-gradient-to-tl from-[hsl(15,70%,60%)] to-[hsl(200,55%,50%)]",
+  "bg-gradient-to-tr from-[hsl(140,60%,65%)] to-[hsl(310,55%,45%)]",
+  "bg-gradient-to-bl from-[hsl(210,65%,60%)] to-[hsl(40,60%,50%)]",
 ];
 
 const getGradient = (id: string) =>
-  gradients[Hash.string(id) % gradients.length];
+  gradients[Math.abs(Hash.string(id)) % gradients.length];
 
 function createCustomMemberSort(userId: string, ownerId: string) {
   return function (a: Member, b: Member) {
@@ -74,17 +71,10 @@ type MemberDropdownMenuProps = PropsWithChildren<{
 }>;
 
 function MemberDropdownMenu(props: MemberDropdownMenuProps) {
-  const StyledItem = ({
-    disabled: _,
-    ...rest
-  }: ComponentProps<typeof DropdownMenuItem>) => (
-    <DropdownMenuItem
-      disabled
-      className="text-xs uppercase font-medium w-32"
-      {...rest}
-    >
-      Settle
-    </DropdownMenuItem>
+  const settle = { enabled: false };
+
+  const StyledItem = (props: ComponentProps<typeof DropdownMenuItem>) => (
+    <DropdownMenuItem className="text-xs uppercase" {...props} />
   );
 
   return (
@@ -99,11 +89,14 @@ function MemberDropdownMenu(props: MemberDropdownMenuProps) {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        {props.balance !== 0 && (
-          <StyledItem disabled onClick={props.settle}>
+        {
+          <StyledItem
+            disabled={!settle.enabled || props.balance !== 0}
+            onClick={props.settle}
+          >
             Settle
           </StyledItem>
-        )}
+        }
         {props.isOwner && !props.isAuthenticatedUser && (
           <StyledItem variant="destructive" onClick={props.remove}>
             Remove
@@ -111,6 +104,96 @@ function MemberDropdownMenu(props: MemberDropdownMenuProps) {
         )}
       </DropdownMenuContent>
     </DropdownMenu>
+  );
+}
+
+type Data = { nickname: string };
+
+const schemas = {
+  nickname: v.pipe(
+    v.pipe(
+      v.string(),
+      v.minLength(constraints.nickname.minLength),
+      v.maxLength(constraints.nickname.maxLength),
+    ),
+  ),
+};
+
+const formSchemaNotStale = (init: string) =>
+  v.pipe(
+    v.object({ nickname: schemas.nickname }),
+    v.check((data) => data.nickname !== init, "These details are already set"),
+  );
+
+type UpdateNicknameFormProps = {
+  group: Group;
+  member: Member;
+  reset: () => void;
+};
+
+function UpdateNicknameForm(props: UpdateNicknameFormProps) {
+  const updateMemberNickname = useUpdateMemberNickname();
+
+  const init = { nickname: props.member.nickname };
+
+  const form = useAppForm({
+    defaultValues: init,
+    validators: { onChange: formSchemaNotStale(init.nickname) },
+    onSubmit: async (options) => {
+      const promise = updateMemberNickname({
+        groupId: props.group.id,
+        nickname: options.value.nickname,
+      });
+
+      return await withToast({
+        promise,
+        finally: props.reset,
+        notify: {
+          loading: "Updating nickname",
+          success: `Group nickname updated to ${options.value.nickname}`,
+          error: "Failed to update nickname",
+        },
+      });
+    },
+  });
+
+  return (
+    <form
+      onSubmit={prevented(() => void form.handleSubmit())}
+      className="flex items-center gap-2"
+    >
+      <form.AppField
+        name="nickname"
+        listeners={{
+          onChange: (ctx) => {
+            if (ctx.value === props.member.nickname) {
+              form.resetField("nickname");
+            }
+          },
+        }}
+        children={(field) => (
+          <field.SheetTextField
+            inputProps={{
+              "aria-label": "Update Nickname",
+              minLength: constraints.nickname.minLength,
+              maxLength: constraints.nickname.maxLength,
+              placeholder: "Enter your username",
+              className: "py-1 px-2.5 h-auto w-auto h-8.5 w-auto flex-initial",
+            }}
+          />
+        )}
+      />
+
+      <form.AppForm>
+        <form.SubmitButton
+          className="uppercase border-border w-22 my-auto py-1.5"
+          size="xs"
+          dirty={{ disableForAria: true }}
+        >
+          Update
+        </form.SubmitButton>
+      </form.AppForm>
+    </form>
   );
 }
 
@@ -123,7 +206,22 @@ type MemberManagementCardProps = {
   remove: (member: Member) => Promise<void>;
 };
 
+function useEditState(defaultState?: "write" | "read") {
+  const [editState, setEditState] = useState<"write" | "read">(
+    defaultState ?? "read",
+  );
+
+  const toggle = () => setEditState((p) => (p === "write" ? "read" : "write"));
+
+  return { state: editState, toggle };
+}
+
 export function MembersList(props: MemberManagementCardProps) {
+  const updateNickname = useUpdateMemberNickname();
+  const editing = useEditState("read");
+  const currentMember = props.members.find((m) => m.userId === props.userId);
+  const [value, setValue] = useState(currentMember?.nickname);
+
   const balances = createBalanceMap(props.expenses); // todo: move to global store
 
   const sortedMembers = props.members.toSorted(
@@ -140,8 +238,11 @@ export function MembersList(props: MemberManagementCardProps) {
   });
 
   return sortedMembers.map(withDerived).map(({ derived, member }) => (
-    <li className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 sm:gap-x-6 py-5 px-2">
-      <Avatar className="size-10 flex-none rounded-lg">
+    <li
+      key={member.userId}
+      className="flex flex-col sm:flex-row sm:justify-between sm:items-center items-center gap-4 sm:gap-x-6 py-5 px-2"
+    >
+      <Avatar className="size-12 flex-none rounded-lg">
         <AvatarFallback
           className={`${getGradient(member.userId)} text-background font-medium text-sm rounded-lg`}
         >
@@ -149,16 +250,24 @@ export function MembersList(props: MemberManagementCardProps) {
         </AvatarFallback>
       </Avatar>
       <div className="min-w-0 flex-auto">
-        <div className="flex items-center gap-2">
-          <p className="text-base font-semibold text-foreground lowercase">
-            {member.nickname}
-          </p>
-          {derived.isAuthenticatedUser && (
-            <Badge variant="theme" className="text-xs h-min">
-              you
-            </Badge>
-          )}
-        </div>
+        {editing.state === "write" && derived.isAuthenticatedUser ? (
+          <UpdateNicknameForm
+            reset={editing.toggle}
+            group={props.group}
+            member={member}
+          />
+        ) : (
+          <div className="flex items-center gap-2">
+            <p className="text-base border border-transparent py-1 font-semibold text-foreground lowercase">
+              {member.nickname}
+            </p>
+            {derived.isAuthenticatedUser && (
+              <Badge variant="theme" className="text-xs h-min ml-1">
+                you
+              </Badge>
+            )}
+          </div>
+        )}
         <span className="text-xs text-muted-foreground uppercase">
           {derived.isOwner ? "owner" : "member"}
         </span>
@@ -170,19 +279,18 @@ export function MembersList(props: MemberManagementCardProps) {
               {getBalanceLabel(balances.get(member.userId))}
             </p>
           )}
-          <div
-            className={`text-base font-semibold w-fit ${getBalanceStyle(derived.balance)}`}
-          >
-            {getBalanceText(derived.balance, { fallback: "settled" })}
-          </div>
+          <p className={`text-base w-fit ${getBalanceStyle(derived.balance)}`}>
+            {getBalanceText(derived.balance)}
+          </p>
         </div>
         {derived.isAuthenticatedUser ? (
           <Button
             variant="outline"
             size="xs"
             className="uppercase border-border w-22"
+            onClick={editing.toggle}
           >
-            Edit
+            {editing.state === "write" ? "Cancel" : "Edit"}
           </Button>
         ) : (
           <MemberDropdownMenu
