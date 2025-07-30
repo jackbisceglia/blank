138343598ce8c446de60e03bf2742249d3103999
jackbisceglia <jackbisceglia2000@gmail.com>
2025-07-29 23:48:20 -0400

    fix(misx): prepare for prod build

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -8,11 +8,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 const root = path.dirname(fileURLToPath(import.meta.url));
 
+console.log("node env from vite: ", process.env.NODE_ENV);
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsconfigPaths(),
     tanstackStart({
+      target: "aws-lambda",
       tsr: {
         routesDirectory: "./src/pages",
         generatedRouteTree: "./src/routes.generated.ts",
