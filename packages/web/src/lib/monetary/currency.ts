4a0cb9cd4636bb188d6bb7927ca6898e0876d55a
jackbisceglia <jackbisceglia2000@gmail.com>
2025-08-12 02:52:05 -0400

    fix:

--- packages/web/src/lib/monetary/currency.ts
+++ packages/web/src/lib/monetary/currency.ts
@@ -1,5 +1,3 @@
-import { pipe, String } from "effect";
-
 export function formatUSDString(amount: number) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -7,8 +5,4 @@ export function formatUSDString(amount: number) {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount);
-}
-
-export function formatUSDFormField(amount: string) {
-  return pipe(amount, parseFloat, formatUSDString, String.slice(1));
 }
